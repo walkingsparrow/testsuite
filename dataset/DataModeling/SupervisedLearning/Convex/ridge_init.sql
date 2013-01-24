e640f21219952c636cb9d1ca86299f3ad4abd433
Hai Qian <Hai.Qian@emc.com>
2013-01-24 10:33:34 -0800

    Test for Ridge regression: 15 data sets used for testing

--- dataset/DataModeling/SupervisedLearning/Convex/ridge_init.sql
+++ dataset/DataModeling/SupervisedLearning/Convex/ridge_init.sql
@@ -12,21 +12,24 @@ declare
     tbl_output varchar := madlib.__cv_unique_string();
 begin
     execute 'select madlib.ridge_newton_train(
-        $_valString$'|| tbl_source ||'$_valString$,
+        $_valString$madlibtestdata.'|| tbl_source ||'$_valString$,
         $_valString$'|| col_ind_var ||'$_valString$,
         $_valString$'|| col_dep_var ||'$_valString$,
-        $_valString$'|| tbl_output ||'$_valString$,
+        $_valString$madlibtestdata.'|| tbl_output ||'$_valString$,
         '|| lambda_value ||',
         '|| normalization ||'
     )';
 
     execute 'select abs(t1.log_likelihood - t2.log_likelihood)
-    from '|| tbl_output ||' t1, '|| tbl_r_result ||' as t2
+    from
+        madlibtestdata.'|| tbl_output ||' t1,
+        '|| tbl_r_result ||' as t2
     where '|| lambda_value ||' = t2.lambda
-    and t1.normalization = t2.normalization'
+    and t1.normalization = t2.normalization
+    and t2.dataset = $_valString$'|| tbl_source ||'$_valString$'
     into rst;
 
-    execute 'drop table if exists '|| tbl_output;
+    execute 'drop table if exists madlibtestdata.'|| tbl_output;
     return rst;
 end;
 $$ language plpgsql;
